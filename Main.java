9ee8b6a1f9f243dd5b43e522f107daabd650ee5b
Aishah1q <148686153+Aishah1q@users.noreply.github.com>
2023-10-26 14:57:52 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,173 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner input=new Scanner(System.in);
+ //1
+      /*  System.out.println("please enter 2 number");
+        int num1=input.nextInt();
+        int num2=input.nextInt();
+
+        System.out.println(num1+num2);
+        System.out.println(num1-num2);
+        System.out.println(num1*num2);
+        System.out.println(num1/num2);
+        System.out.println(num1%num2);
+        System.out.println(num1-num2);*/
+//2
+      /*  System.out.println("please enter number");
+        int num3=input.nextInt();
+        for(int i=1;i<10;i++){
+            System.out.println(num3*i);}*/
+//3
+        //double pi=3.14;
+     /*   System.out.println("please enter number");
+        double num4=input.nextDouble();
+        double Perimeter=2*Math.PI*num4;
+        double Area=Math.PI*num4*num4;
+        System.out.println("Permeter is :"+Perimeter+"Area is :"+Area);*/
+
+
+//4
+
+       /* int a,b,c,sum;
+        double Avereg;
+
+        System.out.println("please enter numbers");
+        a =input.nextInt();
+        System.out.println("please enter numbers");
+        b=input.nextInt();
+        System.out.println("please enter numbers");
+        c=input.nextInt();
+        sum=a+b+c;
+        Avereg=sum/3;
+
+        System.out.println("Averge of the number is :"+Avereg);*/
+
+ //5
+
+
+       /* System.out.println("please enter first number");
+        int num5 =input.nextInt();
+        System.out.println("please enter  second numbers");
+        int num6 =input.nextInt();
+        System.out.println("please enter third numbers");
+        int num7=input.nextInt();
+             if (num7==num5+num6) {
+                 System.out.println(true);
+             }
+             else System.out.println(false);*/
+
+//6
+     /*   System.out.println("please Enter word");
+        String text= input.nextLine();
+        StringBuilder text1 = new StringBuilder(text);
+        System.out.println(text1.reverse());*/
+
+//7
+    /*    System.out.println("please Enter number");
+        int num8=input.nextInt();
+        if(num8%2==0){
+
+            System.out.println("the number is even");}
+            else
+        { System.out.println("the number is odd");
+
+        }*/
+ //8
+        /*double fahrenheit = 0.0;
+        System.out.println("enter  temperature ");
+        double  Centigrade=input.nextDouble();
+
+        fahrenheit = ( Centigrade * 1.8) + 32;
+        System.out.println(" value of temperature in fahrenheit:" + fahrenheit);*/
+//9
+     /*   System.out.println("enter  text ");
+        String strText=input.next();
+        System.out.println("enter  index ");
+        int index1=input.nextInt();
+        System.out.printf(String.valueOf(strText.charAt(index1)));*/
+
+  //10
+       /*double Area,Perimeter1;
+        System.out.println("enter  Width ");
+        double width=input.nextDouble();
+        System.out.println("enter  Height ");
+        double height=input.nextDouble();
+        Area=width*height;
+        Perimeter1=(width+height)*2;
+        System.out.println(Area);
+    System.out.println(Perimeter1);*/
+//11
+        /*System.out.println("enter  number ");
+        int numb1=input.nextInt();
+        System.out.println("enter  number ");
+        int numb2=input.nextInt();
+        if(numb1==numb2)
+
+            System.out.println(numb1+" = "+numb2);
+          if (numb1 !=numb2)
+            System.out.println(numb1+" ! = "+numb2);
+
+          if (numb1>numb2)
+        System.out.println(numb1+" > "+numb2);*/
+
+ //12
+
+       /* System.out.println("enter seconds");
+       int  seconds=input.nextInt();
+        int s=seconds%60;
+        int h=seconds/60;
+        int m=h%60;
+        h=h/60;
+        System.out.println(  h + " hours " + m + " minutes " + s + "seconds ");*/
+
+//13
+       /* System.out.println("enter  number ");
+        int numb3=input.nextInt();
+        System.out.println("enter  number ");
+        int numb4=input.nextInt();
+        System.out.println("enter  number ");
+        int numb5=input.nextInt();
+        System.out.println("enter  number ");
+        int numb6=input.nextInt();
+        if(numb3==numb4&&numb4==numb5&&numb5==numb6) {
+            System.out.println("Numbers are equal");
+        }
+        else{
+            System.out.println("Numbers are not equal");
+        }*/
+//14
+       /* System.out.println("enter  number ");
+        int numb7=input.nextInt();
+        if(numb7>0){
+            System.out.println("Number are Postive");
+        }
+        else if(numb7<0){
+            System.out.println("Number are Negative");}
+        else
+            System.out.println("Numbers is Zero");*/
+//16
+        /*System.out.println("please Enter word");
+        int num9= input.nextInt();
+        int reverse=0;
+        while(num9!=0){
+            int digit=num9%10;
+            reverse=reverse*10+digit;
+        num9/=10;}
+            System.out.println(reverse);*/
+
+ 
+
+
+
+        }}
+
+
+
+
+
+
+
+
